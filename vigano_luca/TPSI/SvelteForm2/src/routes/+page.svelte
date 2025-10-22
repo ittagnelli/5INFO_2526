03231c4143256fabe3d2106015aa5498ea24ee08
Vigano-Luca <luca.vigano'@istitutoagnelli.it>
2025-10-22 10:56:04 +0200

    consegna esercizio 2

---
+++ vigano_luca/TPSI/SvelteForm2/src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let data;
+</script>
+
+<h1>Welcome to SvelteKit FORM Example</h1>
+
+<form method="POST">
+    <div><label for="name">Nome</label></div>
+    <div><input type="text" id="nome" name="nome" size="40"></div>
+    <div><label for="cogome">Cognome</label></div>
+    <div><input type="text" id="cognome" name="cognome" size="40"></div>
+    <div><label for="eta">Età</label></div>
+    <div><input type="number" id="eta" name="eta" min="15"></div>
+    <div><input type="submit"></div>
+</form>
+
+<ul>
+{#each data.utenti as utente}
+    <li>{utente.nome} {utente.cognome} ha {utente.eta} anni</li>
+{/each}
+</ul>
+
+<style>
+    form {
+        width: 50%;
+        display: grid;
+        grid-template-columns: 1fr 4fr;
+        row-gap: 15px;
+        }
+    input, label {
+        height: 25px;
+        border-radius: 6px;
+    }
+    label {
+        line-height: 25px;
+    }
+</style>
