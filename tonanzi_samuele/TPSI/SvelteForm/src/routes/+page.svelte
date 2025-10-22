1337505d47dc4c37a2c1d88e41ce490b1bc50abe
Tonanzi-Samuele <samuele.github@gmail.com>
2025-10-22 09:53:46 +0200

    es01

--- tonanzi_samuele/TPSI/SvelteForm/src/routes/+page.svelte
+++ tonanzi_samuele/TPSI/SvelteForm/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script>
+    export let data; // riceve i dati forniti dal backend durante il load della pagina
+</script>
+
+<h1>Welcome to SvelteKit FORM Example</h1>
+
+<ul> <!-- Visualizza una lista di utenti ricevuti dal back end-->
+{#each data.utenti as utente} 
+    <li>{utente}</li>
+{/each}
+</ul>
