e01a4d986c84b99f349eea481793f4cc2093973d
Romano-Gabriele <gabriele.romano@istitutoagnelli.it>
2025-10-22 10:42:27 +0200

    - aggiunta esercitazione 02 svelteForm

--- TPSI/svelteForm/src/routes/+page.server.js
+++ TPSI/svelteForm/src/routes/+page.server.js
@@ -1,6 +1,21 @@
 export const csr = true; // csr attivo
 export const ssr = true; //ssr attivo
 
+export const actions = {
+    default: async ({cookies, request}) => {
+        const data = await request.formData();
+        console.log("I VALORI DEL FORM SONO:", data);
+
+        utenti.push(
+            {
+                nome: data.get('nome'),
+                cognome: data.get('cognome'),
+                eta: data.get('eta')
+            }
+        )
+    }
+}
+
 export function load({ params }) {
     console.log("ESECUIONE FUNZIONE LOAD:", Date.now());
 
--- TPSI/svelteForm/src/routes/+page.svelte
+++ TPSI/svelteForm/src/routes/+page.svelte
@@ -4,9 +4,37 @@
 
 <h1>Welcome to SvelteKit FORM Example</h1>
 
+<form method="POST">
+    <div><label for="nome">Nome</label></div>
+    <div><input type="text" id="nome" name="nome" size="40"></div>
+    <div><label for="cognome">Cognome</label></div>
+    <div><input type="text" id="cognome" name="cognome" size="40"></div>
+    <div><label for="eta">Età</label></div>
+    <div><input type="text" id="eta" name="eta" size="40"></div>
+    <div><input type="submit"></div>
+</form>
+
 <ul>
     <!-- Visualizza una lista di utenti ricevuti dal back end-->
     {#each data.utenti as utente}
         <li>{utente}</li>
     {/each}
 </ul>
+
+<style>
+    form {
+        width: 50%;
+        display: grid;
+        grid-template-columns: 1fr 4fr;
+        row-gap: 15px;
+    }
+
+    input, label {
+        height: 25px;
+        border-radius: 6px;
+    }
+
+    label {
+        line-height: 25px;
+    }
+</style>
