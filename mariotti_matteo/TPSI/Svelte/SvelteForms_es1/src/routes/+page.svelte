dddcdf6147bbf263357b8dd76765038f191d62c5
Mariotti-Matteo <matteo.mariotti@istitutoagnelli.it>
2025-10-22 10:55:35 +0200

    Rename Es 1

---
+++ mariotti_matteo/TPSI/Svelte/SvelteForms_es1/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let data; // riceve i dati forniti dal backend durante il load della pagina
+</script>
+
+<h1>Welcome to SvelteKit FORM Example</h1>
+
+
+<ul> <!-- Visualizza una lista di utenti ricevuti dal back end-->
+{#each data.utenti as utente} 
+    <li>{utente}</li>
+{/each}
+</ul>
