cfe29e9b89b679b235637032b8ec567c1e803064
Massano-Francesco <massano.francesco@istitutoagnelli.it>
2025-10-22 10:54:53 +0200

    consegna esercitazione 02

--- massano_francesco/SVELTE/SvelteForm/src/routes/+page.server.js
+++ massano_francesco/SVELTE/SvelteForm/src/routes/+page.server.js
@@ -1,19 +1,28 @@
-export const csr = true; // csr attivo
-export const ssr = true; //ssr attivo
+let utenti = [
+    { nome: 'Mario', cognome: 'Rossi', eta: 25 },
+    { nome: 'Luca', cognome: 'Bianchi', eta: 15 },
+    { nome: 'Gianni', cognome: 'Verdi', eta: 19 },
+    { nome: 'Pino', cognome: 'Silvedetre', eta: 27 }
+
+];
 
 export function load({ params }) {
     console.log("ESECUIONE FUNZIONE LOAD:", Date.now());
-    
-    // creo la lista degli utenti. Qui potrei accedere al DB
-    let utenti = [
-        'Mario',
-        'Luca',
-        'Gianni',
-        'Pino',
-        'Marco'
-        
-    ];
 
-    // restituiamo la lista degli utenti
     return { utenti }
-}
+};
+
+export const actions = {
+    default: async ({cookies, request}) => {
+        const data = await request.formData();
+        console.log('I VALORI DEL FORM SONO: ', data);
+
+        utenti.push(
+            {
+                nome: data.get('nome'),
+                cognome: data.get('cognome'),
+                eta: data.get('eta')
+            }
+        )
+    }
+};
