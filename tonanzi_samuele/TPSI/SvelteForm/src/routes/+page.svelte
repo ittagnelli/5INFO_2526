9ba2131bb3f649e4b43c31edb2993fcc8a595a51
Tonanzi-Samuele <samuele.github@gmail.com>
2025-10-22 11:03:11 +0200

    es02

--- tonanzi_samuele/TPSI/SvelteForm/src/routes/+page.svelte
+++ tonanzi_samuele/TPSI/SvelteForm/src/routes/+page.svelte
@@ -5,8 +5,34 @@
 
 <h1>Welcome to SvelteKit FORM Example</h1>
 
-<ul> <!-- Visualizza una lista di utenti ricevuti dal back end-->
-{#each data.utenti as utente} 
-    <li>{utente}</li>
-{/each}
+<form method="POST">
+    <div><label for="nome">Nome</label></div>
+    <div><input type="text" name="nome" id="nome" siz="48"></div>
+    <div><label for="cognome">Cognome</label></div>
+    <div><input type="text" name="cognome" id="cognome" siz="48"></div>
+    <div><label for="eta">Eta</label></div>
+    <div><input type="number" name="eta" id="eta" min="15"></div>
+    <div><input type="submit"></div>
+</form>
+
+<ul>
+    {#each data.utenti as utente}
+        <li>{utente.nome} {utente.cognome} ha {utente.eta} anni</li>     
+    {/each}
 </ul>
+
+<style>
+    form{
+        width: 50%;
+        display: grid;
+        grid-template-columns: 1fr 4fr;
+        row-gap: 15px;
+    }
+    input, label {
+        height: 25px;
+        border-radius: 6px;
+    }
+    label{
+        line-height: 25px;
+    }
+</style>
