2867516e1d01baf31d4a57a9df776bc64c82ddb1
Timis-Vasile <vasile.timis@istitutoagnelli.it>
2025-10-29 09:06:19 +0100

    consegna

--- timis_vasile/TPSI/SvelteForm/src/routes/+page.server.js
+++ timis_vasile/TPSI/SvelteForm/src/routes/+page.server.js
@@ -23,12 +23,21 @@ export const actions = {
         const data = await request.formData();
         console.log("I VALORI DEL FORM SONO:", data );
 
-        utenti.push(
-            {
+        const user = {
                 nome:data.get("nome"),
                 cognome: data.get("cognome"),
                 eta: data.get("eta")
             }
-        )
+    if (user.nome && user.cognome && user.eta) {
+
+        utenti.push(user);
     }
+    else {
+        return{
+            form_error: true,
+            form_vals: user
+
+        }
+    }
+},
 };
