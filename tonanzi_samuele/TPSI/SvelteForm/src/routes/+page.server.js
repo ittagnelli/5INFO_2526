9cda41c9eb200f6ce8cc8e5681e1f045a7b005b0
Tonanzi-Samuele <samuele.github@gmail.com>
2025-10-29 08:59:17 +0100

    es03

--- tonanzi_samuele/TPSI/SvelteForm/src/routes/+page.server.js
+++ tonanzi_samuele/TPSI/SvelteForm/src/routes/+page.server.js
@@ -29,12 +29,19 @@ export const actions = {
     default: async ({cookies, request}) => {
         const data = await request.formData();
         console.log("I VALORI DEL FORM SONO:", data);
-        utenti.push(
-            {
-                nome: data.get("nome"),
-                cognome: data.get("cognome"),
-                eta: data.get("eta")
+        const user = {
+            nome: data.get("nome"),
+            cognome: data.get("cognome"),
+            eta: data.get("eta")
+        }
+        if (user.nome && user.cognome && user.eta){
+            utenti.push(user)
+        }
+        else {
+            return {
+                form_error:true,
+                form_vals: user
             }
-        )
-    }
+        }
+    },
 };
