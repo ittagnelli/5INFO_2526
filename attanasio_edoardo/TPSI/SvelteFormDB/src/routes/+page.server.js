01ccf08ae6d6080e0504010b57c5665467e57e06
Attanasio-Edoardo <edoardo.attanasio@istitutoagnelli.it>
2025-11-12 09:46:14 +0100

    esercitazioni Svelte back-end 1-3

---
+++ attanasio_edoardo/TPSI/SvelteFormDB/src/routes/+page.server.js
@@ -0,0 +1,37 @@
+export const csr = true; // csr attivo
+export const ssr = true; //ssr attivo
+
+
+let utenti = [];
+
+export function load({ params }) {
+    console.log("ESECUIONE FUNZIONE LOAD:", Date.now());
+    
+
+    return {
+        utenti
+    }
+}
+
+export const actions = {
+    default: async ({cookies, request}) => {
+        const data = await request.formData();
+        console.log("I VALORI DEL FORM SONO: ", data)
+
+        const user = {
+            nome: data.get("nome"),
+            cognome: data.get("cognome"),
+            eta: data.get("eta")
+        }
+        if (user.nome && user.cognome && user.eta) {
+            utenti.push(user)
+        } else {
+            return {
+                form_error: true,
+                form_vals: user
+            }
+        }
+        
+    }
+
+};
