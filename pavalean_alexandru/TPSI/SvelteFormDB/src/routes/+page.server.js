7e16c7f82959ca473cff8b088370f6cf33b26836
Pavalean-Alexandru <alexandru.pavalean@istitutoagnelli.it>
2025-11-12 09:32:57 +0100

    consegna esercizio 3 DB

---
+++ pavalean_alexandru/TPSI/SvelteFormDB/src/routes/+page.server.js
@@ -0,0 +1,43 @@
+export const csr = true; // csr attivo
+export const ssr = true; //ssr attivo
+
+let utenti = [
+    {nome: "Mario", cognome: "Rossi",eta:25},
+    {nome: "Luca", cognome: "Bianchi",eta:15},
+    {nome: "Gianni", cognome: "Verdi", eta:19},
+    {nome: "Pino", cognome: "RossSilvedtrei",eta:27},
+
+];
+
+export function load({params}) {
+    console.log("ESECUZIONE FUNZIONE LOAD:", Date.now());
+
+    return {
+        utenti
+    }
+}
+
+
+export const actions = {
+    default: async ({cookies,request}) => {
+        const data = await request.formData();
+        console.log("I VALORI DEL FORM SONO:", data );
+
+        const user = {
+                nome:data.get("nome"),
+                cognome: data.get("cognome"),
+                eta: data.get("eta")
+            }
+    if (user.nome && user.cognome && user.eta) {
+
+        utenti.push(user);
+    }
+    else {
+        return{
+            form_error:true,
+            form_vals:user
+
+        }
+    }
+},
+};
