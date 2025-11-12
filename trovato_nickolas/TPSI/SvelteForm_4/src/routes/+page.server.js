d200a43038d41069cebf0f120da7a8b48252edeb
Trovato-Nickolas <154589467+Trovato-Nickolas@users.noreply.github.com>
2025-11-12 11:03:46 +0100

    add es 4

---
+++ trovato_nickolas/TPSI/SvelteForm_4/src/routes/+page.server.js
@@ -0,0 +1,39 @@
+import Database from 'better-sqlite3'
+const db = new Database("USER.db",{verbose: console.log})
+
+export function load({ params }) {
+    console.log("ESECUIONE FUNZIONE LOAD:", Date.now());
+
+    const query1 = db.prepare("SELECT * FROM Utente");
+
+    const res1 = query1.all();
+
+    return { utenti:res1 };
+};
+
+export const actions = {
+    default: async ({cookies,request}) => {
+        const data = await request.formData();
+        console.log("I VALORI DEL FORM SONO : ",data)
+
+        const user  = {
+            
+                nome: data.get('nome'),
+                cognome: data.get('cognome'),
+                eta: data.get('eta')
+            
+        }
+
+        if(user.nome && user.cognome && user.eta){
+            const query1 = db.prepare("INSERT INTO Utente (nome,cognome,eta) VALUES(? ,? ,?)");
+
+            const res1 = query1.run(user.nome,user.cognome,user.eta);
+            
+        } else {
+            return {
+                form_error: true,
+                form_vals: user
+            }
+        }
+    }
+};
