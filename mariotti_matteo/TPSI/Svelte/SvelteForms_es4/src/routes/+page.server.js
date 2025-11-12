942848ced2856c5d6fbe6c51a2a8ed3e58b33470
Mariotti-Matteo <matteo.mariotti@istitutoagnelli.it>
2025-11-12 10:52:51 +0100

    Es 04

--- mariotti_matteo/TPSI/Svelte/SvelteForms_es4/src/routes/+page.server.js
+++ mariotti_matteo/TPSI/Svelte/SvelteForms_es4/src/routes/+page.server.js
@@ -1,44 +1,34 @@
-// import database from "better-sqlite3";
-// const db = new database("USER.db", {verbose: console.log});
+// export const csr = true; // csr attivo
+// export const ssr = true; //ssr attivo
 
+let utenti = [
+    {nome: 'Mario', cognome: "Rossi", eta:25},
+    {nome: 'Luca', cognome: "Bianchi", eta:15},
+    {nome: 'Gianni', cognome: "Verdi", eta:19},
+    {nome: 'Pino', cognome: "Silvedtre", eta:27}
 
+];
 
-// export function load({ params }) {
-//     console.log("ESECUZIONE FUNZIONA LOAD: ", Date.now());
-//     const query1 = db.prepare("SELECT * FROM Utente");
-//     const res1 = query1.all();
+export function load({ params }) {
+    console.log("ESECUIONE FUNZIONE LOAD:", Date.now());
+    
+    // creo la lista degli utenti. Qui potrei accedere al DB
 
-//     return {
-//         utenti: res1,
-//     };
-// }   
+    // restituiamo la lista degli utenti
+    return { utenti }
+}
 
+export const actions = {
+    default: async({cookies, request}) => {
+        const data = await request.formData();
+        console.log("I valori del Form sono: ", data);
 
-// // export const actions = {
-//     // default: async({cookies, request}) => {
-//         // const data = await request.formData();
-//         // console.log("CREATE ACTION");
-//         // console.log("I valori del Form sono: ", data);
-// // 
-//         // const query2 = db.prepare("INSERT INTO Utente (nome, cognome, eta) VALUES (@nome, @cognome, @anni)");
-//         // const user = {
-//                 // nome: data.get('nome'),
-//                 // cognome:data.get('cognome'),
-//                 // eta: data.get('eta')
-//             // 
-//         // }
-//         // if (user.nome && user.cognome && user.eta){
-//             // const res2 = query2.run({
-//                 // nome: user.nome,
-//                 // cognome: user.cognome,
-//                 // anni: +user.eta
-//             // })
-//         // } else {
-//             // return {
-//                 // form_error: true,
-//                 // form_vals: user
-//             // }
-//         // }
-//     // }
-// // };
-
+        utenti.push(
+            {
+                nome: data.get('nome'),
+                cognome:data.get('cognome'),
+                eta: data.get('eta')
+            }
+        )
+    }
+};
