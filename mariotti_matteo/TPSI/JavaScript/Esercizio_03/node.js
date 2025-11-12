a72b2aeaf4b22ffb096bb2802485c2138f66784f
Mariotti-Matteo <matteo.mariotti@istitutoagnelli.it>
2025-11-12 09:10:15 +0100

    Es 03

--- mariotti_matteo/TPSI/JavaScript/Esercizio_03/node.js
+++ mariotti_matteo/TPSI/JavaScript/Esercizio_03/node.js
@@ -3,15 +3,24 @@ import Database from 'better-sqlite3';
 
 const db = new Database('USER.db');
 
-const query1 = db.prepare('SELECT * FROM Utente'); 
+const query1 = db.prepare('SELECT * FROM Utente where eta >= 18 or eta == 16'); 
 
 
-const res = query1.all(); //esegue SELECT * FROM Utente
+const res = query1.all();//eta >= 18 || eta == 16); //esegue SELECT * FROM Utente
 
 
 const query4 = db.prepare('INSERT INTO Utente (nome, cognome, eta) VALUES(?, ?, ?)');
-const info4 = query4.run('Luca', 'Rossi', 22);
+const info4 = query4.run('Marco', 'Rossi', 22);
+const info5 = query4.run('Luca', 'Rossi', 17);
+const info6 = query4.run('Marco', 'Verdi', 16);
 
+console.log(res);
+
+// const info4 = query4.run[
+//   { id: 1, nome: 'Mario', cognome: 'Rossi', eta: 18 },
+//   { id: 2, nome: 'Giuseppe', cognome: 'Verdi', eta: 17 },
+//   { id: 3, nome: 'Pietro', cognome: 'Bianchi', eta: 19 }
+// ]
 
 // import Database from 'better-sqlite3';
 // 
@@ -24,4 +33,4 @@ const info4 = query4.run('Luca', 'Rossi', 22);
 // console.table(results);
 // 
 // db.close();
-// 
+//  
