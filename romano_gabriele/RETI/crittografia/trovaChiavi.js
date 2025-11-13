1120b0133461b32c22f068656f51d0ac29ae9f66
Romano-Gabriele <gabriele.romano@istitutoagnelli.it>
2025-11-13 11:56:47 +0100

    - aggiunta esercizio crittografia 1

---
+++ romano_gabriele/RETI/crittografia/trovaChiavi.js
@@ -0,0 +1,32 @@
+function trovaDivisori(num) {
+    let arrayDivisori = [];
+    for(let i = 2; i < num; i++) {
+        if(num % i == 0)
+            arrayDivisori.push(num / i);
+    }
+
+    return arrayDivisori;
+}
+
+let p = process.argv[2];
+let q = process.argv[3];
+
+let n = p * q;
+let m = (p - 1) * (q - 1);
+
+let i = m - 1;
+
+while(trovaDivisori(i).length != 0) {
+    i--;
+}
+
+let e = i;
+let k = 0;
+// let k = (m * h + 1) / e;
+
+while((e * k) % m != 1 || e == k) {
+    k++;
+}
+
+console.log("pubblica", e, n);
+console.log("privata", k, n);
