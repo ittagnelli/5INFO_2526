07c4f14a67e1ec10447978b31fccab0f351896ed
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2025-11-19 09:26:52 +0100

    rimosso commenti inutili

--- andruetto_lorenzo/TPSI/SvelteFormDB/src/routes/+page.server.js
+++ andruetto_lorenzo/TPSI/SvelteFormDB/src/routes/+page.server.js
@@ -42,21 +42,6 @@ export const actions = {
     }
 };
 
-// let utenti = [
-//     {nome: 'Mario', cognome: 'Rossi', eta:25},
-//     {nome: 'Luca', cognome: 'Bianchi', eta:15},
-//     {nome: 'Gianni', cognome: 'Verdi', eta:19},
-//     {nome: 'Pino', cognome: 'Silvedtre', eta:27},
-// ];
-
-// export function load ({ params }) {
-//     console.log("ESECUZIONE FUNZIONE LOAD:", Date.now());
-
-//     return {
-//         utenti
-//     }
-// }
-
 // export const actions = {
 //     default: async ({cookies, request}) => {
 //         const data = await request.formData();
